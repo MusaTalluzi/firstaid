903c338f88e8643b1e592a57c02dfe72654b2e6b
Musa <musa.talluzi@gmail.com>
2018-01-21 10:14:02 -0500

    Integrated external api

--- functions/index.js
+++ functions/index.js
@@ -5,64 +5,61 @@ const DialogflowApp = require('actions-on-google').DialogflowApp;
 const functions = require('firebase-functions');
 const rp = require('request-promise');
 const TEST_API_ACTION = 'test_api';
+//var https = require('https');
+//var http = require('http');
 
 /*
 var googleMapsClient = require('@google/maps').createClient({
   key: 'AIzaSyDxWwjS_aPPG4t9Visk1LR4nEkiqLOvmaA'
 });
+
+function burnHandler(app) {
+	var options = {
+		host: 'reqres.in/',
+		path: '/api/users/2',
+	};
+
+	var req = http.get(options, function(res) {
+		var bodyChunks = [];
+		res.on('data', function(chunk) {
+			bodyChunks.push(chunk);
+		}).on('end', function() {
+			var body = Buffer.concat(bodyChunks);
+			console.log('BODY: ' + body);
+			app.tell('Your data has been received!');
+		})
+	});
+
+	req.on('error', function(e) {
+		console.log('ERROR: ' + e.message);
+	});
+ 
+	app.ask("We're handling your request");
+}
 */
-	
 exports.firstaid = functions.https.onRequest((request, response) => {
 	const app = new DialogflowApp({request, response});
 	console.log('Request headers:' + JSON.stringify(request.headers));
 	console.log('Request body:' + JSON.stringify(request.body));
 	
-	function test_api(app) {
-			app.ask('Handling your test_api action');
-			let query = {
-				method: 'GET',
-				uri: 'https://reqres.in/api/users/2'
-			};
-			rp(query).then((error, response, body) => {
-				if(error) {
-					console.log('Error');
-				} else {
-					console.log("this is a string");
-					console.log(JSON.parse(body).data.id);
-					app.tell(JSON.parse(body).data.first_name);
-				}
-		});
+	function burnHandler(app) {
 		
+		//app.ask('Handling your test_api action');
+		let query = {
+			uri: 'https://reqres.in/api/users/2',
+			json: true
+		}
+		rp(query).then((body) => {
+			console.log(body.data);
+			app.tell('Recived a response to your request');
+		}).catch((error) => {
+			app.tell('An error happened');
+			console.log(error);
+		});
 	}
 
 	let actionMap = new Map();
-	actionMap.set(TEST_API_ACTION, test_api);
+	actionMap.set(TEST_API_ACTION, burnHandler);
 	
 	app.handleRequest(actionMap);
 });
-
-
-/*
-const NAME_ACTION = 'make_name';
-const COLOR_ARGUMENT = 'color';
-const NUMBER_ARGUMENT = 'number';
-
-exports.sillyNameMaker = functions.https.onRequest((request, response) => {
-	const app = new App({request, response});
-	console.log('Request headers:' + JSON.stringify(request.headers));
-	console.log('Request body:' + JSON.stringify(request.body));
-	
-	function makeName(app) {
-		let number = app.getArgument(NUMBER_ARGUMENT);
-		let color = app.getArgument(COLOR_ARGUMENT);
-		app.tell('Alright, your silly name is ' +
-			color + ' ' + number +
-			'! I hope you like it. See you next time.');
-	}
-	
-	let actionMap = new Map();
-	actionMap.set(NAME_ACTION, makeName);
-	
-	app.handleRequest(actionMap);
-});
-*/
